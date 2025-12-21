f269909c2edaaafb5718ddfc4da4fe84b7e18470
Mark Phillipson <MPhillipson0@Gmail.com>
2025-12-21 10:12:44 +0000

    feat: add Game1 class for backward compatibility with PacmanGame 🕹️

---
+++ PacmanVoice/Game1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PacmanVoice;
+
+[Obsolete("Use PacmanGame instead. Game1 is retained for backward compatibility.")]
+public class Game1 : PacmanGame
+{
+    public Game1() : base() { }
+}
+
